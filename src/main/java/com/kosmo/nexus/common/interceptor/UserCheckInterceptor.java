cee429177679325d6aae1cdbea4f9220a39ee20b
conteK13 <kangk9428@naver.com>
2024-11-23 14:56:11 +0900

    ky-role check

---
+++ src/main/java/com/kosmo/nexus/common/interceptor/UserCheckInterceptor.java
@@ -0,0 +1,35 @@
+package com.kosmo.nexus.common.interceptor;
+
+import com.kosmo.nexus.dto.LoginDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class UserCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res,
+                             Object handler)
+            throws ServletException, IOException {
+        HttpSession ses = req.getSession();
+        LoginDTO loginUser = (LoginDTO) ses.getAttribute("loginUser");
+        if(!loginUser.getMemberRole().equals("User")){
+            res.setContentType("text/html; charset=UTF-8");  // 응답의 콘텐츠 타입 설정
+            res.getWriter().write("<script type=\"text/javascript\">"
+                    + "alert('유저만 이용 가능합니다.');"
+                    + "history.back();"
+                    + "</script>");
+            return false;  // 인터셉터에서 false를 반환하여 컨트롤러로 넘어가지 않도록 함
+        }// ------------------
+        return true;
+
+    }
+}
--- src/main/java/com/kosmo/nexus/controller/UserController.java
+++ src/main/java/com/kosmo/nexus/controller/UserController.java
@@ -1,35 +1,55 @@
 package com.kosmo.nexus.controller;
 
+import com.kosmo.nexus.dto.LoginDTO;
 import com.kosmo.nexus.dto.MemberDTO;
 import com.kosmo.nexus.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    // /user/myPage
+    @GetMapping("/myPage")
+    public String findMyPage(HttpSession ses, Model model){
+//        session에 담겨 있는 객체를 확인하는 코드
+//        Enumeration<String> attributeNames = session.getAttributeNames();
+//        while (attributeNames.hasMoreElements()) {
+//            String attributeName = attributeNames.nextElement();
+//            log.info("세션 속성: {} = {}",attributeName, session.getAttribute(attributeName));
+//        }
 
-    @GetMapping("/user/myPage")
-    public String findMyPage(@RequestParam("id") String id, Model model){
-        log.info("id ==============={}", id);
-        if(id == "") return "redirect:/index";
+        // 세션에서 loginUser 객체 가져오기
+        LoginDTO loginUser = (LoginDTO) ses.getAttribute("loginUser");
+        if (loginUser == null) {
+            // 로그아웃 코드 추가해야 할지 이야기(종원)
+            return "redirect:/";  // 로그인 페이지로 리다이렉트
+        }
+        String memberId = loginUser.getMemberId();
+        log.info("id ==============={}", memberId);
+        if (memberId == null) {     // memberId가 없는 경우
+            // 로그아웃 코드 추가해야 할지 이야기(종원)
+            return "redirect:/";    // 로그인 페이지로 리다이렉트
+        }
 
-        MemberDTO member = userService.findMember(id);
+        MemberDTO member = userService.findMember(memberId);
         model.addAttribute("member", member);
         log.info("MemberDTO====={}", member);
         return "/user/myPage";
     }
 
-    @PostMapping("/user/myPage")
+    @PostMapping("/myPage")
     public String updateMyPage(MemberDTO member){
         log.info("member==={}",member);
         int result = userService.updateMember(member);

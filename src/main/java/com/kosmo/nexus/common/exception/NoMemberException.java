3e7538a74eef1e18ffb80762420c224b5070c839
jongwon <jwsoshw@naver.com>
2024-11-23 15:00:46 +0900

    jw_signup

--- src/main/java/com/kosmo/nexus/common/exception/NoMemberException.java
+++ src/main/java/com/kosmo/nexus/common/exception/NoMemberException.java
@@ -1,4 +1,4 @@
-package com.kosmo.nexus.exception;
+package com.kosmo.nexus.common.exception;
 //사용자 정의 예외 - 회원이 아닐 경우 발생시킨다(아이디 또는 비밀번호가 틀릴경우)
 public class NoMemberException extends Exception {
 
---
+++ src/main/java/com/kosmo/nexus/controller/SignupController.java
@@ -0,0 +1,23 @@
+package com.kosmo.nexus.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class SignupController {
+
+    @GetMapping("/bSignup")
+    public String bSignupForm(){
+
+        return "member/bSignup";
+    }
+
+    @GetMapping("/pSignup")
+    public String pSignupForm(){
+
+        return "member/pSignup";
+    }
+
+}
